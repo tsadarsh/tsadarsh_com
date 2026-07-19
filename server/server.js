b3ab8b664c60e72d978e9bc1c29e8e3252c34cfb
Adarsh Sriuma <tsadarsh0707@gmail.com>
2026-07-18 18:00:22 -0700

    fixed render build error

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ const wss = new WebSocket.Server({ server });
 // serve built client if exists
 const distPath = path.join(__dirname, '..', 'dist');
 app.use(express.static(distPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 
